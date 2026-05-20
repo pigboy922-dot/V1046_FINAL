84a73b3e12ad8418811fbed898940d7ae22c8ccb
pigboy922-dot <pigboy922@gmail.com>
2026-05-20 20:26:06 +0800

    Clean old files and deploy split-buttons version

--- run_local_real.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d %~dp0
-python v1046_cloud_daily_risk_guard.py
-pause
--- run_local_update_us.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-set V1046_UPDATE_MARKET=US
-python v1046_cloud_daily_risk_guard.py
-pause
